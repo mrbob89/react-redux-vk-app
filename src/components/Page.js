990d0253cbfc29ca3be2e4c94bc9c60ab508d4a9
Roman Boboshko <romanboboshko89@gmail.com>
2016-09-13 14:15:52 +0300

    Create first actions (choose year)

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,15 +1,26 @@
 import React, { PropTypes, Component } from 'react'
 
 export default class Page extends Component {
+  onYearBtnClick(e) {
+    this.props.setYear(+e.target.textContent)
+  }
+
   render() {
     const { year, photos } = this.props
     return <div>
-      <p>You have { photos.length } photoes in { year }</p>
+      <p>
+        <button onClick={::this.onYearBtnClick}>2016</button>
+        <button onClick={::this.onYearBtnClick}>2015</button>
+        <button onClick={::this.onYearBtnClick}>2014</button>
+      </p>
+      <h3>{year} year</h3>
+      <p>You have {photos.length} photoes.</p>
     </div>
   }
 }
 
 Page.propTypes = {
   year: PropTypes.number.isRequired,
-  photos: PropTypes.array.isRequired
+  photos: PropTypes.array.isRequired,
+  setYear: PropTypes.func.isRequired
 }
